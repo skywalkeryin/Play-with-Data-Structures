842667e063dc46ce0d88db4fc4442bc9ca4a5eee
skywalker <skywalkeryin007@gmail.com>
2019-09-22 18:52:32 +0800

    add array add, delete, find and so on functions

--- Java/02_Arrays/src/Array.java
+++ Java/02_Arrays/src/Array.java
@@ -10,6 +10,7 @@ public class Array {
     }
 
     public Array(){
+
         this(10);
     }
 
@@ -18,11 +19,132 @@ public class Array {
     }
 
     public int getSize(){
-             return size;
+
+        return size;
     }
     
     public int getCapacity(){
+
         return  data.length;
+    }
+
+    public int get(int index){
+        if(index < 0 || index >= size ){
+            throw new IllegalArgumentException("Illegal index argument.");
+        }
+        return  data[index];
+    }
+
+    // 修改index索引位置的元素为e
+    public int set(int index, int e){
+        if(index < 0 || index >= size ){
+            throw new IllegalArgumentException("Illegal index argument.");
+        }
+        return  data[index] = e;
+    }
+
+    public boolean contains(int e){
+        for (int i = 0; i < size; i++){
+            if(data[i] == e){
+                return  true;
+            }
+        }
+        return  false;
+    }
+    // find array if contains element e, if not return -1.
+    public int find(int e){
+        for (int i = 0; i < size; i++){
+            if(data[i] == e){
+                return  i;
+            }
+        }
+        return  -1;
+    }
+
+    // add an element at first position
+    public void addFirst(int e){
+        add(0, e);
+    }
+
+    // add an element at last position
+    public void addLast(int e){
+        add(size, e);
+    }
+
+    // add an
+    public void add(int index, int e){
+
+        if(size == data.length) {
+            throw new IllegalArgumentException("Add failed. The array is full.");
+        }
+
+        //避免插入的元素造成数组的不连续
+        if(index < 0 || index > size ){
+           throw new IllegalArgumentException("Illegal index argument.");
+        }
+
+        for (int i = size - 1; i >= index; i--){
+            data[i + 1] = data[i];
+        }
+
+        data[index] = e;
+        size ++;
+    }
+    public int removeFirst(){
+        return remove(0);
+    }
+    public int removeLast(){
+        return remove(size - 1);
+    }
+
+    // 从数组删除元素e
+    public void removeElement(int e){
+        int index = find(e);
+        if(index != -1) {
+            remove(index);
+        }
+    }
+
+    public void removeAllElement(int e){
+        int index = find(e);
+        if(index != -1) {
+            for(int i = 0; i < size; i++){
+                if(data[i] == e){
+                    remove(i);
+                }
+            }
+        }else{
+            System.out.println("There is no " + e + " in the array");
+
+           // throw new IllegalArgumentException("There is no this element in the array");
+        }
+    }
+
+    public int remove(int index){
+
+        if(index < 0 || index >= size ){
+            throw new IllegalArgumentException("Illegal index argument.");
+        }
+        int result = data[index];
+        for(int i = index + 1; i < size; i++){
+            data[i-1] = data[i];
+        }
+        size--;
+        return result;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Array: size=%d , capacity=%d\n", size, data.length));
+        sb.append("[ ");
+        for (int i = 0; i < size; i ++) {
+            sb.append(data[i]);
+            sb.append(", ");
+        }
+        sb.append("] ");
+       return  sb.toString();
     }
 
 
--- Java/02_Arrays/src/Main.java
+++ Java/02_Arrays/src/Main.java
@@ -2,5 +2,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Array newArray = new Array(20);
+
+        for (int i =0; i < 10; i++){
+            newArray.addLast(i);
+        }
+        System.out.println(newArray);
+
+        newArray.addFirst(3);
+        newArray.addFirst(3);
+        newArray.add(2, 3);
+
+        newArray.set(2, 4);
+
+        System.out.println(newArray);
+
+        newArray.removeLast();
+        newArray.removeFirst();
+
+        System.out.println(newArray);
+
+        newArray.removeAllElement(100);
+
+
+
+
     }
 }
