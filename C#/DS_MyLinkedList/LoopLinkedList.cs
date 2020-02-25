17d40316adc43cee8d9c035662c786459059fb9e
skywalkeryin <skywalkeryin007@gmail.com>
2020-02-25 23:38:25 +0800

    finish loop linked list

--- C#/DS_MyLinkedList/LoopLinkedList.cs
+++ C#/DS_MyLinkedList/LoopLinkedList.cs
@@ -32,7 +32,8 @@ namespace DS_MyLinkedList
             }
         }
 
-        private Node dummyHead, tail;
+        private Node dummyHead;
+        //private Node  tail;
         private int size;
 
         public LoopLinkedList()
@@ -67,13 +68,14 @@ namespace DS_MyLinkedList
                 prev = prev.next;
             }
 
+            //空链表 和 链表尾情况
             if (prev.next == null || prev.next == dummyHead)
             {
                 dummyHead.prev = temp;
                 temp.next = dummyHead;
                 temp.prev = prev;
                 prev.next = temp;
-                tail = temp;
+               // tail = temp;
             }
             else
             {
@@ -126,6 +128,117 @@ namespace DS_MyLinkedList
         public void AddLast(T val)
         {
             Add(size, val);
+        }
+        // O(1) 复杂度
+        public void AddLast2(T val)
+        {
+            Node temp = new Node(val, null, null);
+
+            if (dummyHead.prev == null) //空链表
+            {
+                dummyHead.prev = temp;
+                temp.next = dummyHead;
+                temp.prev = dummyHead;
+                dummyHead.next = temp;
+            }
+            else
+            {
+                Node tail = dummyHead.prev;
+                temp.next = dummyHead;
+                temp.prev = tail;
+
+                tail.next = temp;
+                dummyHead.prev = temp;
+            }
+            size++;
+
+        }
+        public T Get(int index)
+        {
+            if (index < 0 || index >= size )
+            {
+                throw new Exception("Get Failed. Illegal index.");
+            }
+            Node cur = dummyHead.next;
+            for (int i = 0; i< index; i++)
+            {
+                cur = cur.next;
+            }
+            return cur.e;
+        }
+
+        public T GetFirst()
+        {
+            return Get(0);
+        }
+
+        public T GetLast()
+        {
+            return Get(size - 1);
+        }
+
+        public T Remove(int index)
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Remove Failed. LoopLinkedlist is empty.");
+            }
+            if (index < 0 || index >= size)
+            {
+                throw new Exception("Remove Failed. Illegal index.");
+            }
+            Node prev = dummyHead;
+            for (int i = 0; i < index; i++)
+            {
+                prev = prev.next;
+            }
+
+            Node delNode = prev.next;
+            T ret = delNode.e;
+
+            if (delNode.next == dummyHead) // 链表尾
+            {
+                prev.next = dummyHead;
+                dummyHead.prev = prev;
+            }
+            else
+            {
+                prev.next = prev.next.next;
+                prev.next.next.prev = prev;
+            }
+            // delete node
+            delNode.next = null;
+            delNode.prev = null;
+            size--;
+            return ret;
+        }
+        public T RemoveFirst()
+        {
+            return Remove(0);
+        }
+        public T RemoveLast()
+        {
+            return Remove(size - 1);
+        }
+        public T RemoveLast2()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Remove Failed. LoopLinkedlist is empty.");
+            }
+
+            Node prev = dummyHead.prev.prev;
+            Node tail = dummyHead.prev;
+            Node ret = prev.next;
+
+            prev.next = dummyHead;
+            dummyHead.prev = prev;
+
+            tail.next = null;
+            tail.prev = null;
+            size--;
+
+            return ret.e;
         }
         public override string ToString()
         {
--- C#/DS_MyLinkedList/Program.cs
+++ C#/DS_MyLinkedList/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace DS_MyLinkedList
 {
     class Program
     {
+        static double TestLoopLinkedList1(LoopLinkedList<int> list, int OptCount)
+        {
+            Random rnd = new Random();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < OptCount; i++)
+            {
+                list.AddLast(rnd.Next(int.MaxValue));
+            }
+            for (int i = 0; i < OptCount; i++)
+            {
+                list.RemoveLast2();
+            }
+            stopWatch.Stop();
+            return (double)stopWatch.ElapsedMilliseconds / 1000.000;
+        }
+
+        static double TestLoopLinkedList2(LoopLinkedList<int> list, int OptCount)
+        {
+            Random rnd = new Random();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < OptCount; i++)
+            {
+                list.AddLast2(rnd.Next(int.MaxValue));
+            }
+            for (int i = 0; i < OptCount; i++)
+            {
+                list.RemoveLast2();
+            }
+            stopWatch.Stop();
+            return (double)stopWatch.ElapsedMilliseconds / 1000.000;
+        }
+
         static void Main(string[] args)
         {
             //MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            //linkedList.RemoveFirst();
             //for(int i = 0; i < 5; i++)
             //{
             //    linkedList.AddFirst(i);
@@ -29,19 +65,32 @@ namespace DS_MyLinkedList
             //head = Remove_Linked_List_Elements.RemoveElements(head, 6, 0);
 
             // LoopLinkedList
+            //LoopLinkedList<int> list3 = new LoopLinkedList<int>();
+            //Console.WriteLine(list3);
+
+
+            //LoopLinkedList<int> list2 = new LoopLinkedList<int>();
+
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    list2.AddLast2(i);
+            //}
+            //Console.WriteLine(list2);
+
+            //list2.RemoveLast2();
+            //list2.RemoveLast2();
+            //Console.WriteLine(list2);
+
+            //test addlast 时间复杂度
             LoopLinkedList<int> list3 = new LoopLinkedList<int>();
-            Console.WriteLine(list3);
+            LoopLinkedList<int> list4 = new LoopLinkedList<int>();
+            int optCount = 100000;
 
 
-            LoopLinkedList<int> list2 = new LoopLinkedList<int>();
-
-            for (int i = 0; i < 5; i++)
-            {
-                list2.AddLast(i);
-            }
-            list2.Add(2, 8);
-            Console.WriteLine(list2);
-
+            double testTime3 = TestLoopLinkedList1(list3, optCount);
+            Console.WriteLine($"Linkedlist 3, Time: {testTime3}s");
+            double testTime4 = TestLoopLinkedList2(list4, optCount);
+            Console.WriteLine($"Linkedlist 4, Time: {testTime4}s");
 
         }
     }
